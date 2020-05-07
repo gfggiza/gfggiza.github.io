8ee32132fba58b49081e9407ee479c339b49b95f
Gisele Granato <gi__@live.com>
2020-05-06 23:55:39 -0300

    lastest version - javascript simplificado

--- main.js
+++ main.js
@@ -1,44 +1,25 @@
+function HideMenu() {
+    document.getElementById("menu").classList.add('hide');
+}
+
+function DisplayMenu() {
+    document.getElementById("menu").classList.remove('hide');
+}
+
+function DisplayPage(el) {
+    HideMenu();
+    document.getElementById(el.dataset.menuItem).classList.remove('hide');
+}
+
+function HidePage(el) {
+    DisplayMenu();
+    document.getElementById(el.dataset.menuItem).classList.add('hide');
+}
+
 function AbrirModal(){
     document.getElementById("modal").classList.remove('hide');
 }
 
 function FecharModal(){
     document.getElementById("modal").classList.add('hide');
-}
-
-function AbrirPortifolio(){
-    document.getElementById("menu").classList.add('hide');
-    document.getElementById("portifolio").classList.remove('hide');
-}
-
-function AbrirProjetos(){
-    document.getElementById("menu").classList.add('hide');
-    document.getElementById("projetos").classList.remove('hide');
-}
-
-function AbrirSobremim(){
-    document.getElementById("menu").classList.add('hide');
-    document.getElementById("sobremim").classList.remove('hide');
-}
-
-function AbrirContato(){
-    document.getElementById("menu").classList.add('hide');
-    document.getElementById("contato").classList.remove('hide');
-}
-
-function VoltarPortifolio(){
-    document.getElementById("portifolio").classList.add('hide');
-    document.getElementById("menu").classList.remove('hide');
-}
-function VoltarProjetos(){
-    document.getElementById("projetos").classList.add('hide');
-    document.getElementById("menu").classList.remove('hide');
-}
-function VoltarSobremim(){
-    document.getElementById("sobremim").classList.add('hide');
-    document.getElementById("menu").classList.remove('hide');
-}
-function VoltarContato(){
-    document.getElementById("contato").classList.add('hide');
-    document.getElementById("menu").classList.remove('hide');
 }
